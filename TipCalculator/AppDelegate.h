7426f012fc368a6a2928a1cc598fecd15030148d
Zane Salim <zsalim@twitter.com>
2014-05-05 14:30:45 -0700

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Zane Salim on 5/5/14.
+//  Copyright (c) 2014 zanesalim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
